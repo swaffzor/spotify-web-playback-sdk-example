18ffb83422a5125f2fdf8c3b29d61af8db4b778c
Jeremy Swafford <jeremy@swafford.co>
2024-10-25 12:56:19 -0400

    tacks on current minute to player name to distinguish between different instances

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -30,7 +30,7 @@ function WebPlayback(props) {
         window.onSpotifyWebPlaybackSDKReady = () => {
 
             const player = new window.Spotify.Player({
-                name: 'Web Playback SDK',
+                name: `Composer POC ${new Date().getMinutes()}`,
                 getOAuthToken: cb => { cb(props.token); },
                 volume: 0.5
             });
